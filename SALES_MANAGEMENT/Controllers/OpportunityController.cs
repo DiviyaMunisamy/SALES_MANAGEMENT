2516862f1e626c0497fba5443d73030d5a262c06
SUGUMARGOVINDAN <sugumar.govindan@twilightitsolutions.com>
2021-01-05 12:51:01 +0530

    Adding Opportunity

---
+++ SALES_MANAGEMENT/Controllers/OpportunityController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SALES_MANAGEMENT.Controllers
+{
+    public class OpportunityController : Controller
+    {
+        private SqlConnection con;
+        //To Switch Connection String................................//
+        private void Connection()
+        {
+            string constring = ConfigurationManager.ConnectionStrings["LeadConnection"].ToString();
+            con = new SqlConnection(constring);
+        }
+        // GET: Opportunity
+        public ActionResult Create(OpportunityModel model, HttpPostedFileBase file)
+        {
+            
+            {
+                Connection();
+                SqlCommand Command = new SqlCommand("SP_Opportunity_Insert", con);
+                Command.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                Command.Parameters.AddWithValue("@Topic", model.Topic);
+                Command.Parameters.AddWithValue("@Contact", model.Contact);
+                Command.Parameters.AddWithValue("@Account", model.Account);
+                Command.Parameters.AddWithValue("@PurchaseTimeForm", model.PurchaseTimeForm);
+                Command.Parameters.AddWithValue("@Currency", model.Currency);
+                Command.Parameters.AddWithValue("@BudgetAmount", model.BudgetAmount);
+                Command.Parameters.AddWithValue("@PurchesProcess", model.PurchesProcess);
+                Command.Parameters.AddWithValue("@ForecastCategory", model.ForecastCategory);
+                Command.Parameters.AddWithValue("@Description", model.Description);
+                Command.Parameters.AddWithValue("@CurrentSuitation", model.CurrentSuitation);
+                Command.Parameters.AddWithValue("@CustommerNeed", model.CustommerNeed);
+                Command.Parameters.AddWithValue("@ProposedSolution", model.ProposedSolution);          
+                Command.ExecuteNonQuery();
+                con.Close();
+                ViewBag.Message = "SAVED SUCCESSFULLY :)";
+                return View();
+            }
+
+        }
+    }
+}
---
+++ SALES_MANAGEMENT/Models/OpportunityModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SALES_MANAGEMENT.Models
+{
+    public class OpportunityModel
+    {
+        [Display(Name = "Topic")]
+        [Required(ErrorMessage = " Topic is Required")]
+        public string Topic { get; set; }
+
+        [Display(Name = "Contact")]
+        [Required(ErrorMessage = " Contact is Required")]
+        public string Contact { get; set; }
+
+        [Display(Name = "Account")]
+        [Required(ErrorMessage = " LAccount is Required")]
+        public string Account { get; set; }
+
+        [Display(Name = "PurchaseTimeForm")]
+        [Required(ErrorMessage = " PurchaseTimeForm is Required")]
+        public string PurchaseTimeForm { get; set; }
+
+        [Display(Name = "Currency")]
+        [Required(ErrorMessage = " Currency is Required")]
+        public string Currency { get; set; }
+
+        [Display(Name = "BudgetAmount")]
+        [Required(ErrorMessage = " BudgetAmount is Required")]
+        public string BudgetAmount { get; set; }
+
+        [Display(Name = "PurchesProcess")]
+        [Required(ErrorMessage = " PurchesProcess is Required")]
+        public string PurchesProcess { get; set; }
+
+        [Display(Name = "ForecastCategory")]
+        [Required(ErrorMessage = " ForecastCategory is Required")]
+        public string ForecastCategory { get; set; }
+
+        [Display(Name = "Description")]
+        [Required(ErrorMessage = " Description is Required")]
+        public string Description { get; set; }
+
+        [Display(Name = "CurrentSuitation")]
+        [Required(ErrorMessage = " CurrentSuitation is Required")]
+        public string CurrentSuitation { get; set; }
+
+        [Display(Name = "CustommerNeed")]
+        [Required(ErrorMessage = "CustommerNeed is Required")]
+        public string CustommerNeed { get; set; }
+
+        [Display(Name = "ProposedSolution")]
+        [Required(ErrorMessage = " ProposedSolution is Required")]
+        public string ProposedSolution { get; set; }
+    }
+}
