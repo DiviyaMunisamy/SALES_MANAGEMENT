7ca3f611bac415202cda3e33a5e0acfbfe4e021c
Puthuvarasan <puthuvarasan@twilightits.com>
2021-01-18 12:28:14 +0530

    changes

---
+++ SALES_MANAGEMENT/Reports/Report.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Reporting.WebForms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SALES_MANAGEMENT
+{
+    public partial class Report : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ReportViewer1.ProcessingMode = ProcessingMode.Local;
+                ReportViewer1.LocalReport.ReportPath = Server.MapPath("Report.rdlc");
+                SaleEntities entities = new SaleEntities();
+                ReportDataSource datasource = new ReportDataSource("TBL_Leads", (from Lead in entities.TBL_Leads.Take(50)
+                                                                                 select Lead));
+                ReportViewer1.LocalReport.DataSources.Clear();
+                ReportViewer1.LocalReport.DataSources.Add(datasource);
+            }
+        }
+    }
+}
